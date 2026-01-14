950c79654c2322b7359acd71de0a9dd610e3559f
poold3 <dallinpool@gmail.com>
2026-01-13 23:56:23 -0500

    Adding player data to local storage.

--- src/app/services/local-storage/local-storage.ts
+++ src/app/services/local-storage/local-storage.ts
@@ -3,7 +3,8 @@ import { Injectable } from "@angular/core";
 export enum LocalStorageKey {
   WORLD_INFO_SETTINGS = "worldInfoSettings",
   MAP_SETTINGS = "mapSettings",
-  STATS_SETTINGS = "statsSettings"
+  STATS_SETTINGS = "statsSettings",
+  PLAYER_DATA_SETTINGS = "playersSettings"
 }
 
 @Injectable({
