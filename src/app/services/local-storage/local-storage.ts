9d4cf836b7ed02a224420a6dfd06b8da408bd1ae
poold3 <dallinpool@gmail.com>
2026-02-28 21:43:32 -0500

    Removing unused local storage keys.

--- src/app/services/local-storage/local-storage.ts
+++ src/app/services/local-storage/local-storage.ts
@@ -1,10 +1,8 @@
 import { Injectable } from "@angular/core";
 
 export enum LocalStorageKey {
-  WORLD_INFO_SETTINGS = "worldInfoSettings",
   MAP_SETTINGS = "mapSettings",
   STATS_SETTINGS = "statsSettings",
-  PLAYER_DATA_SETTINGS = "playersSettings",
   ADVANCEMENTS_SETTINGS = "advancementsSettings"
 }
 
